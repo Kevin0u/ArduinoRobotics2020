1d30f323b45fcbd37aeb12638a3c07951332be60
Kevin Ou <kevinou@Kevins-MacBook-Pro.local>
2020-07-19 23:10:05 -0400

    Softpot Membrane Sensor

---
+++ 7.18.20 sensors/Softpot_Membrane_Sensor/Softpot_Membrane_Sensor.ino
@@ -0,0 +1,41 @@
+ const int ledPin = 3;
+ const int SoftPin = A0; //pin A0 to read analog input
+
+ int value;
+ 
+ void setup()
+  {
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(9600);
+  }
+  
+  void loop()
+  {
+    value = analogRead(SoftPin);
+    Serial.println(value);
+    value = map(value, 0, 1023, 0, 255);
+    analogWrite(ledPin, value);
+    delay(100);
+  }
+
+
+
+
+
+
+
+// const int SoftPin = A0; //pin A0 to read analog input
+//
+// int value;
+// 
+// void setup()
+//  {
+//    Serial.begin(9600);
+//  }
+//  
+//  void loop()
+//  {
+//    value = analogRead(SoftPin);
+//    Serial.println(value);
+//    delay(100);
+//  }
